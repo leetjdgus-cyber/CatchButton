859ad364a7afdfd30cf1a3d53162401c2f0ec8dc
성현 이 <leetjdgus@gmail.com>
2026-03-12 10:52:35 +0900

    버튼생성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(295, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 112);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
